2f699bb22d7ae27e3d2b02ee271360a7b99572af
Joseph Dunne <josephdunne1993@gmail.com>
2019-11-24 22:55:10 +0800

    Added favicon. fixed deploy command

--- commands.sh
+++ commands.sh
@@ -4,8 +4,8 @@
 #
 
 SRC_PATH="./src"
-DIST_PATH="./src"
-S3_BUCKET=s3://joseph-dunne.com
+DIST_PATH="./dist"
+S3_BUCKET="s3://joseph-dunne.com"
 
 #
 # Helper functions.
@@ -38,39 +38,6 @@ function command_exists_or_exit {
 # Commands.
 #
 
-function run_local_server {
-  cd $SRC_PATH
-  python3 -m http.server
-}
-
-function compile_less {
-  local file="$1"
-  local dist="$2"
-  lessc "$file" "$dist"
-}
-
-function compile_less_file_to_css_file {
-  local file="$1"
-  local dist=${file%.less}
-  dist=$(echo "${dist}.css")
-  compile_less "$file" "$dist"
-}
-
-function compile_all_less_files_to_css_files {
-  find "$SRC_PATH" -type f -name "*.less" | while read file; do
-    compile_less_file_to_css_file "$file"
-  done
-}
-
-function watch_files_for_changes {
-  command_exists_or_exit "fswatch"
-  fswatch -0 -r -e ".*" -i "\\.less$" "$SRC_PATH" | while read -d "" event
-  do
-    #TODO: Check for delete event.
-    compile_less_file_to_css_file "$event"
-  done
-}
-
 # Starts local version of the website.
 function rundev_command {
   mkdir -p dist
@@ -90,7 +57,7 @@ function build_command {
 
 # Uploads website to s3.
 function deploy_command {
-  aws s3 cp $SRC_PATH $S3_BUCKET --delete --acl public-read
+  aws s3 sync $DIST_PATH $S3_BUCKET --acl public-read --delete
   exit 0
 }
 

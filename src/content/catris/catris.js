09002cd2bf6765a8794ae6f6e887d2d198ca412a
Joseph Dunne <josephdunne1993@gmail.com>
2019-12-22 14:32:05 +0800

    stuff

--- src/content/catris/catris.js
+++ src/content/catris/catris.js
@@ -3,6 +3,14 @@
   catris.js
 */
 
+// Constants.
+const NONE = 0;
+const WALL = 1;
+const CAT_U = 2;
+const CAT_D = 3;
+const CAT_L = 4;
+const CAT_R = 5;
+
 class Catris {
 
   constructor(canvasEl, opts) {
@@ -24,6 +32,7 @@ class Catris {
     this.GRID_HEIGHT = Math.floor(canvasEl.height / this.TILE_SIZE);
     this.cursorX = null;
     this.cursorY = null;
+    this.bloodParticles = [];
 
     this.onRequestAnimationFrame = this.onRequestAnimationFrame.bind(this);
   }
@@ -42,35 +51,34 @@ class Catris {
 
     // Load images.
     this.images = {};
-    this._loadImage(1, './assets/img/wall.png');
-    this._loadImage(2, './assets/img/cat-u.png');
-    this._loadImage(3, './assets/img/cat-d.png');
-    this._loadImage(4, './assets/img/cat-l.png');
-    this._loadImage(5, './assets/img/cat-r.png');
+    this._loadImage(WALL, './assets/img/wall.png');
+    this._loadImage(CAT_U, './assets/img/cat-u.png');
+    this._loadImage(CAT_D, './assets/img/cat-d.png');
+    this._loadImage(CAT_L, './assets/img/cat-l.png');
+    this._loadImage(CAT_R, './assets/img/cat-r.png');
 
     // Init grid.
     this.grid = new Uint8Array(this.GRID_WIDTH * this.GRID_HEIGHT);
 
     // Init bottom with walls.
     for (let x = 0; x < this.GRID_WIDTH; x += 1) {
-      this.setGrid(x, this.GRID_HEIGHT - 1, 1);
+      this.setGrid(x, this.GRID_HEIGHT - 1, WALL);
     }
 
     // Init sides with walls.
     for (let y = 0; y < this.GRID_HEIGHT - 1; y += 1) {
       this.setGrid(0, y, 1);
-      this.setGrid(this.GRID_WIDTH - 1, y, 1);
+      this.setGrid(this.GRID_WIDTH - 1, y, WALL);
     }
 
     // Add 2 random cats to the bottom.
-    this.setGrid(1, this.GRID_HEIGHT - 2, 5);
-    this.setGrid(2, this.GRID_HEIGHT - 2, 4);
-    this.setGrid(4, this.GRID_HEIGHT - 2, 2);
-    this.setGrid(4, this.GRID_HEIGHT - 3, 3);
+    this.setGrid(1, this.GRID_HEIGHT - 2, CAT_R);
+    this.setGrid(2, this.GRID_HEIGHT - 2, CAT_L);
+    this.setGrid(4, this.GRID_HEIGHT - 2, CAT_U);
+    this.setGrid(4, this.GRID_HEIGHT - 3, CAT_D);
 
     // Set position to spawn.
-    this.cursorY = 0;
-    this.cursorX = 4;
+    this.spawnBlock();
     this.toNextDown = 1;
 
     return this;
@@ -142,7 +150,7 @@ class Catris {
     this.lastTime = timestamp;
 
     this.render();
-    this.tick(timestamp - last);
+    this.tick((timestamp - last) * 0.001);
 
     requestAnimationFrame(this.onRequestAnimationFrame);
   }
@@ -153,6 +161,198 @@ class Catris {
   tick(dT) {
     if (dT <= 0) {
       return;
+    }
+
+    this.toNextDown -= dT;
+    if (this.toNextDown <= 0) {
+      this.moveBlockDown();
+    }
+
+    let expired = false;
+    for (let obj of this.bloodParticles) {
+      obj.x -= obj.xv;
+      obj.y -= obj.yv;
+      obj.yv -= dT;
+      obj.t -= dT;
+      if (obj.t <= 0) {
+        expired = true;
+      }
+    }
+    if (expired) {
+      this.bloodParticles = this.bloodParticles.filter(obj => obj.t > 0);
+    }
+  }
+
+  /**
+   * Moves block left. Returns true if it hits a wall.
+   */
+  moveBlockLeft() {
+
+    const a = this.getGrid(this.cursorX, this.cursorY);
+
+    if (this.getGrid(this.cursorX - 1, this.cursorY) === NONE) {
+
+      this.setGrid(this.cursorX - 1, this.cursorY, a);
+
+      if (a === CAT_R) {
+        this.setGrid(this.cursorX, this.cursorY, CAT_L);
+        this.setGrid(this.cursorX + 1, this.cursorY, NONE);
+      } else {
+        this.setGrid(this.cursorX, this.cursorY, NONE);
+      }
+
+      if (a === CAT_U) {
+        this.setGrid(this.cursorX, this.cursorY - 1, NONE);
+        this.setGrid(this.cursorX - 1, this.cursorY - 1, CAT_D);
+      }
+
+      this.cursorX -= 1;
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Moves block left. Returns true if it hits a wall.
+   */
+  moveBlockRight() {
+
+    const a = this.getGrid(this.cursorX, this.cursorY);
+
+    if (
+      this.getGrid(this.cursorX + 1, this.cursorY) === NONE
+      || (
+        a === CAT_R
+        && this.getGrid(this.cursorX + 2, this.cursorY) === NONE
+      )
+    ) {
+
+      this.setGrid(this.cursorX + 1, this.cursorY, a);
+      this.setGrid(this.cursorX, this.cursorY, NONE);
+
+      if (a === CAT_R) {
+        this.setGrid(this.cursorX + 2, this.cursorY, CAT_L);
+      } else if (a === CAT_U) {
+        this.setGrid(this.cursorX, this.cursorY - 1, NONE);
+        this.setGrid(this.cursorX + 1, this.cursorY - 1, CAT_D);
+      }
+
+      this.cursorX += 1;
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Moves the current block downwards.
+   * Returns true if the block hits a wall.
+   */
+  moveBlockDown() {
+
+    let hitWall = false;
+    const a = this.getGrid(this.cursorX, this.cursorY);
+
+    if (
+      this.getGrid(this.cursorX, this.cursorY + 1) !== NONE
+      || (
+        // Horizontal cat check.
+        a === CAT_R
+        && this.getGrid(this.cursorX + 1, this.cursorY + 1) !== NONE
+      )
+    ) {
+
+      this.checkForCompleteRows();
+      this.spawnBlock();
+      hitWall = true;
+
+    } else {
+
+      this.setGrid(this.cursorX, this.cursorY + 1, a);
+      if (a === CAT_U) {
+        this.setGrid(this.cursorX, this.cursorY, CAT_D);
+        this.setGrid(this.cursorX, this.cursorY - 1, NONE);
+      } else {
+        this.setGrid(this.cursorX, this.cursorY, 0);
+      }
+      if (a === CAT_R) {
+        this.setGrid(this.cursorX + 1, this.cursorY + 1, CAT_L);
+        this.setGrid(this.cursorX + 1, this.cursorY, NONE);
+      }
+
+      this.cursorY += 1;
+    }
+
+    this.toNextDown = 1;
+    return hitWall;
+  }
+
+  /**
+   * Spawns some blood at a grid location.
+   */
+  spawnBlood(x, y) {
+    this.bloodParticles.push({
+      x: x * this.TILE_SIZE + (Math.random() * this.TILE_SIZE),
+      y: y * this.TILE_SIZE + (Math.random() * this.TILE_SIZE),
+      xv: (Math.random() - 0.5) * 1,
+      yv: (Math.random() - 0.5) * 1,
+      t: 2,
+    });
+  }
+
+  /**
+   * Kills an entire row.
+   */
+  killRow(y) {
+    for (let x = 1; x < this.GRID_WIDTH - 1; x += 1) {
+      this.setGrid(x, y, NONE);
+      this.spawnBlood(x, y);
+    }
+  }
+
+  /**
+   * Checks for complete rows.
+   */
+  checkForCompleteRows() {
+
+    for (let y = 0; y < this.GRID_HEIGHT - 1; y += 1) {
+
+      let complete = true;
+      for (let x = 1; x < this.GRID_WIDTH - 1; x += 1) {
+        if (this.getGrid(x, y) === NONE) {
+          complete = false;
+          break;
+        }
+      }
+
+      if (complete) {
+        this.killRow(y);
+      }
+    }
+  }
+
+  /**
+   * Spawns a random block.
+   */
+  spawnBlock() {
+
+    this.cursorY = 0;
+    this.cursorX = 4;
+
+    const rand = Math.floor(Math.random() * 2);
+    if (rand === 0) {
+
+      // Horizontal cat.
+      this.setGrid(this.cursorX, this.cursorY, 5);
+      this.setGrid(this.cursorX + 1, this.cursorY, 4);
+
+    } else {
+
+      // Vertical cat.
+      this.setGrid(this.cursorX, this.cursorY, 2);
+      this.setGrid(this.cursorX, this.cursorY - 1, 3);
+
     }
   }
 
@@ -173,6 +373,14 @@ class Catris {
         this.drawGrid(x, y);
       }
     }
+
+    // Draw particles.
+    for (let obj of this.bloodParticles) {
+      ctx.beginPath();
+      ctx.fillStyle = '#ff0000';
+      ctx.rect(obj.x, obj.y, 4, 4);
+      ctx.fill();
+    }
   }
 
   arrowUp() {
@@ -180,18 +388,22 @@ class Catris {
   }
 
   arrowDown() {
-
+    this.moveBlockDown();
   }
 
   arrowLeft() {
-
+    this.moveBlockLeft();
   }
 
   arrowRight() {
-
+    this.moveBlockRight();
   }
 
   spaceBar() {
-    
+    for (let i = 0; i < 256; i += 1) {
+      if (this.moveBlockDown()) {
+        return;
+      }
+    }
   }
 }
